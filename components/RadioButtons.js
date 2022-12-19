251e2ee040b41bdcf3a21825741ae8685b0ed020
Mika.dev <charmikadev@gmail.com>
2022-12-19 15:42:54 +0000

    Project + data setup

---
+++ components/RadioButtons.js
@@ -0,0 +1,10 @@
+
+const RadioButtons = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default RadioButtons;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Radio from '../components/Radio';
+
+export default function Home() {
+    return (
+        <div className='app'>
+            <div>
+                <Radio />
+            </div>
+        </div>
+    )
+}
